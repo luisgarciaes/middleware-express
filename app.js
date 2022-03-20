dc9a43a7269338647915e8884e701bf9fac855c8
ruruisu <zaddozgl@gmail.com>
2022-03-20 17:37:11 -0600

    prueba route

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => {
-    res.send('Hello world, this is the root route woo!🎉');
+    res.send('Hello world, this is the root route woo!🎉');// localhost:3000
 });
 
 app.listen(3000);
 app.get('/uno', (req,res)=>{
-    res.send('Hello world from route One 🥇')
-})
+    res.send('Hello world from route Uno 🥇') // localhost:3000/uno
+
+    
+});
+
+app.get('/prueba', (req,res)=>{
+    res.send('Hello world from route Prueba 🔨') // localhost:3000/prueba
+
+    
+});
